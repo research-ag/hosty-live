7ea97cce238b22f44fa5b8bcaec11564d3627604
andy <andy.gura.jd@gmail.com>
2026-01-30 15:55:51 +0200

    removed supabase dependency

--- src/api/_system/http-client.ts
+++ src/api/_system/http-client.ts
@@ -1,17 +1,10 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-import { supabase } from "../../lib/supabase";
-import { API_BASE_URL } from "../../config";
-
 interface MakeRequestConfig extends AxiosRequestConfig {
   method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 }
 
-export const ACCESS_TOKEN_LS_KEY = "access_token";
-export const REFRESH_TOKEN_LS_KEY = "refresh_token";
-
 const ai = axios.create({
-  baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
@@ -19,19 +12,7 @@ const ai = axios.create({
 });
 
 export const makeRequest = async <D>(config: MakeRequestConfig) => {
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  return ai<D>({
-    ...config,
-    headers: {
-      ...(!!session?.access_token && {
-        Authorization: `Bearer ${session.access_token}`,
-      }),
-      ...config.headers,
-    },
-  });
+  return ai<D>(config);
 };
 
 makeRequest.auto = <D>(config: MakeRequestConfig): Promise<D> =>
--- src/config/config.ts
+++
@@ -1,1 +0,0 @@
-export const API_BASE_URL = `${import.meta.env.VITE_SUPABASE_URL}/functions/v1`;
